73dbc205ac48323f617b0829d7c2a5b21ceecfe6
yuntianming0310 <bk1677594629@gmail.com>
2026-01-17 10:06:18 +0800

    Initial Commit

---
+++ package/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import MarketingApp from './components/MarketingApp'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hi, there!</h1>
+      <br />
+      <MarketingApp />
+    </div>
+  )
+}
