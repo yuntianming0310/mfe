32a94bf97817b447e1440b2704688092d27cfdce
yuntianming0310 <bk1677594629@gmail.com>
2026-01-17 15:26:39 +0800

    feat: try action

--- package/container/src/App.js
+++ package/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp'
 export default function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>329!</h1>
       <br />
       <MarketingApp />
     </div>
