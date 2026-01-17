d94cc0ba7b907f061f9aaba513aa81ef9569a682
yuntianming0310 <bk1677594629@gmail.com>
2026-01-17 23:24:28 +0800

    fix: replace broken unsplash image source with picsum

--- package/marketing/src/components/Landing.js
+++ package/marketing/src/components/Landing.js
@@ -119,7 +119,7 @@ export default function Album() {
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
+                    image={`https://picsum.photos/400/200?random=${card}`}
                     title="Image title"
                   />
                   <CardContent className={classes.cardContent}>
